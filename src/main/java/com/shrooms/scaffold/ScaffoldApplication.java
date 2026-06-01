da2753b725cd87219a23fc724e8af743d5a0e6c4
Cveti <cvetinkavr@gmail.com>
2026-06-01 11:29:49 +0200

    initialize spring boot project

---
+++ src/main/java/com/shrooms/scaffold/ScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.shrooms.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaffoldApplication.class, args);
+	}
+
+}
